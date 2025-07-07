b23a01b95fed1eb23cc35606c0617d8d74a013d7
prifd74 <prifdgamer@gmail.com>
2025-07-07 21:43:48 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+});
+
+client.login(process.env.BOT_TOKEN);
+
+module.exports = client;
